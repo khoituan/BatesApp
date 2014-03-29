4d59a2afcc8f55496c21d4b669ede4feee1ad5f1
khoituan <knguyen@bates.edu>
2014-03-28 22:03:56 -0400

    Nothing new yet, just created another class

---
+++ RSSreader/APPDaysOfWeek.h
@@ -0,0 +1,13 @@
+//
+//  APPDaysOfWeek.h
+//  RSSreader
+//
+//  Created by Khoi Tuan Nguyen on 3/28/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APPDaysOfWeek : UITableViewController
+
+@end
